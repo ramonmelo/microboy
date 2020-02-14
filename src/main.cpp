07082fef91fe411084b501ea38270973dd64a8d8
Ramon Melo <me@ramonmelo.com>
2020-02-14 09:08:14 -0300

    CHANGED: Main script to use Managers
    Also include a little game moving the pixel player

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include "Arduino.h"
 #include "InputManager.h"
-#include "Wire.h"
-#include "SSD1306Wire.h"
+#include "DisplayManager.h"
 
-SSD1306Wire display1(0x3c, D2, D1); // ADDRESS, SDA, SCL
-SSD1306Wire display2(0x3d, D2, D1); // ADDRESS, SDA, SCL
+#define BUZZER_PIN D5
 
 microboy::InputManager inputManager;
+microboy::DisplayManager *displayManager;
+
+int x = 0;
+int y = 0;
+
+void draw()
+{
+	displayManager->setPixel(x, y);
+}
 
 void setup()
 {
@@ -15,34 +22,50 @@ void setup()
 	{
 	}
 
+	pinMode(BUZZER_PIN, OUTPUT);
+	digitalWrite(BUZZER_PIN, LOW);
+
 	Serial.println("\nstarting...");
 
-	// Initialising the UI will init the display too.
-	display1.init();
-	display2.init();
-
-	display1.flipScreenVertically();
-	display1.setFont(ArialMT_Plain_10);
-	display1.setTextAlignment(TEXT_ALIGN_LEFT);
-
-	display2.flipScreenVertically();
-	display2.setFont(ArialMT_Plain_10);
-	display2.setTextAlignment(TEXT_ALIGN_LEFT);
-
-	// display1.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-	// display2.begin(SSD1306_SWITCHCAPVCC, 0x3D);
+	displayManager = new microboy::DisplayManager();
+	displayManager->setup(draw);
 }
 
 void loop()
 {
-	display1.clear();
-	display1.setColor(INVERSE);
-	display1.drawString(0, 0, "Hello world 1: " + String(millis()));
-	display1.display();
+	microboy::Action value = inputManager.read();
 
-	display2.clear();
-	display2.drawString(0, 0, "Hello world 2: " + String(millis()));
-	display2.display();
+	switch (value)
+	{
+	case microboy::UP:
+		y -= 1;
+		break;
+	case microboy::DOWN:
+		y += 1;
+		break;
+	case microboy::LEFT:
+		x -= 1;
+		break;
+	case microboy::RIGHT:
+		x += 1;
+		break;
+	case microboy::ACTION:
+		Serial.println("ACTION");
+		break;
+	default:
+		break;
+	}
+
+	if (x < 0) { x = 0; }
+	if (y < 0) { y = 0; }
+	if (x > MAX_WIDTH) { x = MAX_WIDTH; }
+	if (y > MAX_HEIGHT) { y = MAX_HEIGHT; }
+
+	Serial.print(x);
+	Serial.print(" , ");
+	Serial.println(y);
+
+	displayManager->update();
 
 	delay(10);
 }
