0dc92d7e85eef527343b46b0ad387321ad012cf4
Ramon Melo <me@ramonmelo.com>
2020-02-15 21:19:51 -0300

    ADDED: Drawing Methods

--- lib/MicroBoy/DisplayManager.cpp
+++ lib/MicroBoy/DisplayManager.cpp
@@ -8,6 +8,7 @@ DisplayManager::DisplayManager() : display1(0x3d, D2, D1), display2(0x3c, D2, D1
 	display1.flipScreenVertically();
 	display1.setFont(ArialMT_Plain_10);
 	display1.setTextAlignment(TEXT_ALIGN_LEFT);
+	// display1.invertDisplay();
 	display1.clear();
 	display1.display();
 
@@ -15,6 +16,7 @@ DisplayManager::DisplayManager() : display1(0x3d, D2, D1), display2(0x3c, D2, D1
 	display2.flipScreenVertically();
 	display2.setFont(ArialMT_Plain_10);
 	display2.setTextAlignment(TEXT_ALIGN_LEFT);
+	// display2.invertDisplay();
 	display2.clear();
 	display2.display();
 }
@@ -24,7 +26,7 @@ void DisplayManager::setup(void (*drawFunc)())
 	draw = drawFunc;
 }
 
-void DisplayManager::update()
+void DisplayManager::update(void)
 {
 	display1.clear();
 	display2.clear();
@@ -38,19 +40,79 @@ void DisplayManager::update()
 	display2.display();
 }
 
+// Other functions
+
+void DisplayManager::invertDisplay(void)
+{
+	display1.invertDisplay();
+	display2.invertDisplay();
+}
+
+void DisplayManager::normalDisplay(void)
+{
+	display1.normalDisplay();
+	display2.normalDisplay();
+}
+
 // Draw Funcs
 
 void DisplayManager::setPixel(int16_t x, int16_t y)
 {
-	if (x < HALF_WIDTH)
-	{
-		display1.setPixel(x, y);
-	}
-	else
-	{
-		x -= HALF_WIDTH;
-		display2.setPixel(x, y);
-	}
+	display1.setPixel(x, y);
+	display2.setPixel(x - HALF_WIDTH, y);
+}
+
+void DisplayManager::drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1)
+{
+	display1.drawLine(x0, y0, x1, y1);
+	display2.drawLine(x0 - HALF_WIDTH, y0, x1 - HALF_WIDTH, y1);
+}
+
+void DisplayManager::drawCircle(int16_t x, int16_t y, int16_t radius)
+{
+	display1.drawCircle(x, y, radius);
+	display2.drawCircle(x - HALF_WIDTH, y, radius);
+}
+
+void DisplayManager::fillCircle(int16_t x, int16_t y, int16_t radius)
+{
+	display1.fillCircle(x, y, radius);
+	display2.fillCircle(x - HALF_WIDTH, y, radius);
+}
+
+void DisplayManager::drawRect(int16_t x, int16_t y, int16_t width, int16_t height)
+{
+	display1.drawRect(x, y, width, height);
+	display2.drawRect(x - HALF_WIDTH, y, width, height);
+}
+
+void DisplayManager::fillRect(int16_t x, int16_t y, int16_t width, int16_t height)
+{
+	display1.fillRect(x, y, width, height);
+	display2.fillRect(x - HALF_WIDTH, y, width, height);
+}
+
+void DisplayManager::drawHorizontalLine(int16_t x, int16_t y, int16_t length)
+{
+	display1.drawHorizontalLine(x, y, length);
+	display2.drawHorizontalLine(x - HALF_WIDTH, y, length);
+}
+void DisplayManager::drawVerticalLine(int16_t x, int16_t y, int16_t length)
+{
+	display1.drawVerticalLine(x, y, length);
+	display2.drawVerticalLine(x - HALF_WIDTH, y, length);
+}
+
+void DisplayManager::drawFastImage(int16_t x, int16_t y, int16_t width, int16_t height, const uint8_t *image)
+{
+	display1.drawFastImage(x, y, width, height, image);
+	display2.drawFastImage(x - HALF_WIDTH, y, width, height, image);
+}
+
+void DisplayManager::drawXbm(int16_t x, int16_t y, int16_t width, int16_t height, const uint8_t *xbm)
+{
+	display1.drawXbm(x, y, width, height, xbm);
+	display2.drawXbm(x - HALF_WIDTH, y, width, height, xbm);
 }
 
 } // namespace microboy
--- lib/MicroBoy/DisplayManager.h
+++ lib/MicroBoy/DisplayManager.h
@@ -14,16 +14,29 @@ namespace microboy
 {
 class DisplayManager
 {
-	public:
+public:
 	DisplayManager();
 
-	void setup(void (*drawFunc)());	
-	void update();
+	void setup(void (*drawFunc)());
+	void update(void);
 
 	// Drawing Functions
 	void setPixel(int16_t x, int16_t y);
+	void drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1);
+	void drawCircle(int16_t x, int16_t y, int16_t radius);
+	void fillCircle(int16_t x, int16_t y, int16_t radius);
+	void drawRect(int16_t x, int16_t y, int16_t width, int16_t height);
+	void fillRect(int16_t x, int16_t y, int16_t width, int16_t height);
+	void drawHorizontalLine(int16_t x, int16_t y, int16_t length);
+	void drawVerticalLine(int16_t x, int16_t y, int16_t length);
 
-	private:
+	void drawFastImage(int16_t x, int16_t y, int16_t width, int16_t height, const uint8_t *image);
+	void drawXbm(int16_t x, int16_t y, int16_t width, int16_t height, const uint8_t *xbm);
+
+	void invertDisplay(void);
+	void normalDisplay(void);
+
+private:
 	SSD1306Wire display1; // ADDRESS, SDA, SCL
 	SSD1306Wire display2; // ADDRESS, SDA, SCL
 	void (*draw)();
