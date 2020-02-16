445f3545d9bb230cd7a456cf96d2ec962b3097ad
Ramon Melo <me@ramonmelo.com>
2020-02-15 21:20:22 -0300

    CHANGED: Example of player with image

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
 #include "Arduino.h"
+
 #include "InputManager.h"
 #include "DisplayManager.h"
+
+#include "image.h"
 
 #define BUZZER_PIN D5
 
 microboy::InputManager inputManager;
 microboy::DisplayManager *displayManager;
 
+void draw();
+
 int x = 0;
 int y = 0;
+int curr_frame = 0;
 
-void draw()
-{
-	displayManager->setPixel(x, y);
-}
+microboy::Action lastAction;
 
 void setup()
 {
@@ -31,23 +34,55 @@ void setup()
 	displayManager->setup(draw);
 }
 
+void draw()
+{
+	// displayManager->setPixel(x, y);
+
+	// displayManager->drawCircle(x, y, 4);
+	// displayManager->fillCircle(x, y, 2);
+
+	// displayManager->drawHorizontalLine(x, y, 10);
+	// displayManager->drawVerticalLine(x, y, 10);
+
+	displayManager->drawXbm(x, y, FRAME_WIDTH, FRAME_HEIGHT, frames[curr_frame]);
+
+	//   display->drawXbm(x + 34, y + 14, WiFi_Logo_width, WiFi_Logo_height, WiFi_Logo_bits);
+
+	// displayManager->fillRect(x, y, 50, 50);
+
+	// displayManager->drawLine(23, 14, 213, 50);
+}
+
+void nextFrame()
+{
+	curr_frame += 1;
+	if (curr_frame == NUMBER_FRAMES)
+	{
+		curr_frame = 0;
+	}
+}
+
 void loop()
 {
-	microboy::Action value = inputManager.read();
+	lastAction = inputManager.read();
 
-	switch (value)
+	switch (lastAction)
 	{
 	case microboy::UP:
 		y -= 1;
+		nextFrame();
 		break;
 	case microboy::DOWN:
 		y += 1;
+		nextFrame();
 		break;
 	case microboy::LEFT:
 		x -= 1;
+		nextFrame();
 		break;
 	case microboy::RIGHT:
 		x += 1;
+		nextFrame();
 		break;
 	case microboy::ACTION:
 		Serial.println("ACTION");
@@ -56,16 +91,24 @@ void loop()
 		break;
 	}
 
-	if (x < 0) { x = 0; }
-	if (y < 0) { y = 0; }
-	if (x > MAX_WIDTH) { x = MAX_WIDTH; }
-	if (y > MAX_HEIGHT) { y = MAX_HEIGHT; }
-
-	Serial.print(x);
-	Serial.print(" , ");
-	Serial.println(y);
+	if (x < 0)
+	{
+		x = 0;
+	}
+	if (y < 0)
+	{
+		y = 0;
+	}
+	if (x > MAX_WIDTH)
+	{
+		x = MAX_WIDTH;
+	}
+	if (y > MAX_HEIGHT)
+	{
+		y = MAX_HEIGHT;
+	}
 
 	displayManager->update();
 
-	delay(10);
+	delay(20);
 }
